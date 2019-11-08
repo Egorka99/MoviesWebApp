7ce764e833f868666330a0077d98abd75e564766
Egor <ze99@rambler.ru>
2019-11-08 12:27:28 +0300

    working post request

--- movies/src/main/java/FilmsProject/RESTservice/MovieController.java
+++ movies/src/main/java/FilmsProject/RESTservice/MovieController.java
@@ -2,14 +2,11 @@ package FilmsProject.RESTservice;
 
 import FilmsProject.Interfaces.FilmService;
 import FilmsProject.Interfaces.UserService;
-import FilmsProject.Model.Film;
-import FilmsProject.Model.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.ws.rs.*;
-import java.util.List;
+import javax.ws.rs.core.Response;
 
 @Path("/movie")
 @Consumes("application/json")
@@ -29,10 +26,20 @@ public class MovieController {
         return filmService.getFilmDetails(id);
      }
 
+     //Test data: http://localhost:8081/webapp/movie/addReview?filmIdentifier=326&reviewText=GoodFilm&rating=8.9
+//     @PUT
+//     @Path("/addReview")
+//     public boolean addReview(@FormParam("filmIdentifier") String filmIdentifier,
+//                              @FormParam("reviewText") String reviewText, @FormParam("rating") double rating) {
+//          return userService.writeReview(null,filmIdentifier,reviewText,rating);
+//     }
+
      @POST
-     @Path("/{id}/addReview")
-     public boolean addReview(@String filmIdentifier,String reviewText,double rating) {
-          return userService.writeReview(null,filmIdentifier,reviewText,rating);
+     @Path("/post")
+     public Response createTrackInJSON( @QueryParam("name") String name) {
+
+          String result = "Name : " + name;
+          return Response.status(201).entity(result).build();
      }
 
 
