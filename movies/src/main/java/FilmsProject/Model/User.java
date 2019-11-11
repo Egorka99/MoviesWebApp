e6380fc45bb25c0d21a02a042f487f140d1d567b
Egor <ze99@rambler.ru>
2019-11-11 19:46:17 +0300

    add entities

--- movies/src/main/java/FilmsProject/Model/User.java
+++ movies/src/main/java/FilmsProject/Model/User.java
@@ -1,7 +1,21 @@
 package FilmsProject.Model;
 import org.springframework.stereotype.Component;
 
-public class User extends Person {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
+public class User  {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "login", unique = true)
+    private String login;
+
+    @Column(name="password")
+    private String password;
 
     public User(String userName, String userLogin, String userPassword) {
         this.name = userName;
@@ -10,5 +24,30 @@ public class User extends Person {
     }
 
     public User() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return login.equals(user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
+    }
 
 }
